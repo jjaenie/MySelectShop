179b618f1482108fbd620abf3c55e8f7bc6a8888
jjaenie <wogus990519@gmail.com>
2025-09-23 14:41:32 +0900

    상품 페이징 및 정렬

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.sparta.myselectshop.naver.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,9 +29,14 @@ public class ProductController {
 
     // 관심 상품 조회하기
     @GetMapping("/products")
-    public List<ProductResponseDto> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public Page<ProductResponseDto> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
         // 응답 보내기
-        return productService.getProducts(userDetails.getUser());
+        return productService.getProducts(userDetails.getUser(),  page-1, size, sortBy, isAsc);
     }
 
     // 관심 상품 희망 최저가 등록하기
@@ -40,10 +46,10 @@ public class ProductController {
         return productService.updateProduct(id, requestDto);
     }
 
-    // 관리자 조회
-    @GetMapping("/admin/products")
-    public List<ProductResponseDto> getAllProducts() {
-        return productService.getAllProducts();
-    }
+//    // 관리자 조회
+//    @GetMapping("/admin/products")
+//    public List<ProductResponseDto> getAllProducts() {
+//        return productService.getAllProducts();
+//    }
 
 }
